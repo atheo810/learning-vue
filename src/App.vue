96ae2c55cdb48e9956fb43843dd538023b4011af
atheo810 <atheos810@gmail.com>
2024-02-02 20:08:12 +0800

    fix: style

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,12 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div id="layout">
+  <div id="app">
     <header>
-      <div class="wrapper">
+      <div id="nav">
         <nav>
           <RouterLink :to="{ name: 'event-list' }">Events</RouterLink> |
-          <RouterLink :to="{ name: 'about' }">About</RouterLink>
+          <RouterLink :to="{ name: 'About' }">About</RouterLink>
         </nav>
       </div>
     </header>
@@ -17,25 +17,29 @@ import { RouterLink, RouterView } from 'vue-router'
   </div>
 </template>
 
-<style scoped>
-#layout {
+<style>
+#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-nav {
+
+#nav {
   padding: 30px;
 }
-nav a {
+
+#nav a {
   font-weight: bold;
   color: #2c3e50;
 }
-nav a.router-link-exact-active {
+
+#nav a.router-link-exact-active {
   color: #42b983;
 }
-h2 {
+
+h4 {
   font-size: 20px;
 }
 </style>
