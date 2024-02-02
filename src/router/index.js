23955f7a74971ba5364d78a4c81347b5d5960f3a
atheo810 <atheos810@gmail.com>
2024-02-02 20:08:34 +0800

    feat: added Nested Route

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '../views/EventListView.vue'
-import EventDetailsView from '../views/EventDetailsView.vue'
+import EventDetails from '../views/event/Details.vue'
+import EventLayout from '../views/event/Layout.vue'
+import EventRegister from '../views/event/Register.vue'
+import EventEdit from '../views/event/Edit.vue'
 import AboutView from '../views/AboutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // index
     {
       path: '/',
       name: 'event-list',
@@ -13,15 +17,35 @@ const router = createRouter({
       // making props route and send it to component (params query)
       props: (route) => ({ page: parseInt(route.query.page) || 1 }),
     },
+    // Event List
     {
       path: '/event/:id',
-      name: 'event-details',
+      name: 'event-Layouts',
       props: true,
-      component: EventDetailsView,
+      component: EventLayout,
+      children: [
+        {
+          path: '',
+          name: 'event-Details',
+          component: EventDetails,
+        },
+        // Register
+        {
+          path: 'register',
+          name: 'event-Register',
+          component: EventRegister,
+        },
+        {
+          path: 'edit',
+          name: 'event-Edit',
+          component: EventEdit,
+        },
+      ],
     },
+
     {
       path: '/about',
-      name: 'about',
+      name: 'About',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
