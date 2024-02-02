3056ab032a930aadc0eb8b42d5a44205abb2e2f3
atheo810 <atheos810@gmail.com>
2024-02-02 20:58:47 +0800

    feat: component NetworkError

---
+++ src/views/NetworkError.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="networkError">
+    <h1>Ohh !</h1>
+    <h3>
+      It looks like you're experiencing some network issues, please take a
+      breath and <a href="#" @click="$router.go(-1)">Click Here</a> to try again
+    </h3>
+  </div>
+</template>
