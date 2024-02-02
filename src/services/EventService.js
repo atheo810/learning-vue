901f0c40341c7930d1785be4f1f2a1a95de57a2c
atheo810 <atheos810@gmail.com>
2024-02-02 14:02:50 +0800

    feat: using Api Vue Mastery

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,18 +1,25 @@
 import axios from 'axios'
 
+// making instance axios
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/atheo810/learning-vue',
+  // add baseURL for axios
+  baseURL: 'https://my-json-server.typicode.com/Code-Pop/Touring-Vue-Router',
+  // define credentials
   withCredentials: false,
+  // request header using JSON
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
   },
 })
 
+// export when this Service has been imported
 export default {
-  getEvents() {
-    return apiClient.get('/events')
+  // function have 2 params
+  getEvents(perPage, page) {
+    return apiClient.get('/events?_limit=' + perPage + '&_page=' + page)
   },
+  // just lookin for parameter and pass it to axios baseURL
   getEvent(id) {
     return apiClient.get('/events/' + id)
   },
