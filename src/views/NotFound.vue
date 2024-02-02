d8f4792014ccbe2400b437f75a61dcdabb012cd4
atheo810 <atheos810@gmail.com>
2024-02-02 20:58:25 +0800

    feat: component

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,5 +1,16 @@
 <template>
   <h1>Opps!</h1>
-  <p>The page you're looking for is not here</p>
+  <p>The {{ resource }} you're looking for is not here</p>
   <router-link :to="{ name: 'event-list' }">Back To HomePage</router-link>
 </template>
+<script>
+export default {
+  props: {
+    resource: {
+      required: true,
+      type: String,
+      default: 'page',
+    },
+  },
+}
+</script>
