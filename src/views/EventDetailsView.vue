de59628a391e32e37c787cbc1f3c58fdf46e118a
atheo810 <atheos810@gmail.com>
2024-02-02 14:04:00 +0800

    feat: docs

--- src/views/EventDetailsView.vue
+++ src/views/EventDetailsView.vue
@@ -2,13 +2,16 @@
 import { ref, onMounted } from 'vue'
 import EventService from '@/services/EventService.js'
 
+// received props
 const props = defineProps({
   id: {
     required: true,
   },
 })
+// usinf reactive state
 const event = ref(null)
 
+// if component get mount will do the calling for axios service
 onMounted(() => {
   EventService.getEvent(props.id)
     .then((response) => {
@@ -22,6 +25,7 @@ onMounted(() => {
 
 <template>
   <div v-if="event">
+    <!-- if event no null do the template below -->
     <h1>{{ event.title }}</h1>
     <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
     <p>{{ event.description }}</p>
